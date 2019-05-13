fac35cd08d03bcac1c94bb12932644442f3fda16
dubo <dubo@sinosoft.com.cn>
2019-05-13 17:09:52 +0800

    Initial commit

---
+++ demo12334/src/main/java/com/itmuch/demo12334/Demo12334Application.java
@@ -0,0 +1,13 @@
+package com.itmuch.demo12334;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo12334Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo12334Application.class, args);
+    }
+
+}
